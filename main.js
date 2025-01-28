dd39485b013e9c34269bc6261d07de99083e78ad
gudejoey <jerryyu333@gmail.com>
2025-01-27 23:17:01 -0800

    Link Animation

--- main.js
+++ main.js
@@ -2,15 +2,8 @@ var lastScrollY = window.pageYOffset;
 var nav = document.querySelector(".nav");
 // var logo = document.querySelector(".logo");
 var body = document.getElementById("body");
-const logo = document.getElementById('logo');
+
 var html=document.querySelector("html");
-var circl=document.getElementById('circl');
-const phone = window.matchMedia('(max-width: 480px)').matches;
-const tablet = window.matchMedia('(min-width: 481px) and (max-width: 767px)').matches;
-const touch = window.matchMedia("(pointer: coarse)").matches;
-
-
-
 
 
 
